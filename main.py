eea6d0d6653a1a46d99198d7e502f48f178bfe7f
TheOneWhoIsADemon <nyashnyclown1998@gmail.com>
2024-07-18 14:35:38 +0200

    First_repo

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 print("ЛАСКАВО ПРОСИМО")
 
-print("Hello Git")
+n = 5000
+
+hours = n // (60 * 60)
+minutes = (n - hours * 60 * 60) // 60
+seconds = n - hours * 60 * 60 - minutes * 60
+
+print(hours)
+print(minutes)
+print(seconds)
